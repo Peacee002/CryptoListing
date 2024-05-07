d097aa0a132a1e9e7a5cc704d6e14530090d7796
Barış Köseoğlu <peace@192.168.1.87>
2024-05-07 20:34:13 +0300

    First Commit

---
+++ crpytoApp.swift
@@ -0,0 +1,17 @@
+//
+//  crpytoApp.swift
+//  crpyto
+//
+//  Created by Barış Köseoğlu on 26.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct crpytoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
